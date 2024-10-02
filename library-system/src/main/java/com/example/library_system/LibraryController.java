38833175d72bdc851f822f8ecab96e450879cde2
Jonatan <jonatanav255@hotmail.com>
2024-10-02 09:09:01 -0600

    adding rest controller

--- library-system/src/main/java/com/example/library_system/LibraryController.java
+++ library-system/src/main/java/com/example/library_system/LibraryController.java
@@ -6,7 +6,9 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class LibraryController {
 
     private List<Book> books = new ArrayList<>();

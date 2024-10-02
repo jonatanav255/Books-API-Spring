de12e27dc48adb1f9087f8901809d0aa340ac4b4
Jonatan <jonatanav255@hotmail.com>
2024-10-02 09:02:31 -0600

    adding library controller

---
+++ library-system/src/main/java/com/example/library_system/LibraryController.java
@@ -0,0 +1,25 @@
+package com.example.library_system;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public class LibraryController {
+
+    private List<Book> books = new ArrayList<>();
+
+    // Get all books
+    @GetMapping("/books")
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    @PostMapping("/books")
+    public Book addBook(@RequestBody Book book) {
+        books.add(book);
+        return book;
+    }
+}

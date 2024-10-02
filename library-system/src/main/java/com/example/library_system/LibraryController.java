0a537cc4dc4b3969af49594b87b4785156bb29d9
Jonatan <jonatanav255@hotmail.com>
2024-10-02 10:30:38 -0600

    adding cross origin

--- library-system/src/main/java/com/example/library_system/LibraryController.java
+++ library-system/src/main/java/com/example/library_system/LibraryController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*") // Allow access from any origin
 public class LibraryController {
 
     private List<Book> books = new ArrayList<>();
